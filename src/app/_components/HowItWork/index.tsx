6e3dbae77778f1707383aa197735366f52f5630f
artem schegortzov <tema.chegortzov@mail.ru>
2025-04-20 17:58:23 +0300

    Init styles

--- src/app/_components/HowItWork/index.tsx
+++ src/app/_components/HowItWork/index.tsx
@@ -1,25 +1,28 @@
 import Link from 'next/link'
 import { howItWorkItems } from './constants'
 import { HowItWorkItem } from './types'
+import styles from './index.module.scss'
 
 export const Item = (item: HowItWorkItem, index: number) => {
   return (
-    <article>
-      <span>{`0${index + 1}`}</span>
-      <div>
-        <h3>{item.title}</h3>
-        <p>{item.paragraph}</p>
+    <article className={styles.item}>
+      <span className={styles.item__counter}>{`0${index + 1}`}</span>
+      <div className={styles.item__info}>
+        <h3 className={styles.item__title}>{item.title}</h3>
+        <p className={styles.item__paragraph}>{item.paragraph}</p>
       </div>
-      <Link href={item.link.url}>{item.link.text}</Link>
+      <Link href={item.link.url} className={styles.item__link}>
+        {item.link.text}
+      </Link>
     </article>
   )
 }
 
 export const HowItWork = () => {
   return (
-    <section>
-      <h2>Как это работает</h2>
-      <div>{howItWorkItems.map(Item)}</div>
+    <section className={styles.section}>
+      <h2 className={styles.title}>Как это работает</h2>
+      <div className={styles.items}>{howItWorkItems.map(Item)}</div>
     </section>
   )
 }
