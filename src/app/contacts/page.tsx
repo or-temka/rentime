ffaa27145d0ea43fa23824a7faa5e6ee1da5a7f8
Or-temka <69795850+or-temka@users.noreply.github.com>
2025-04-27 23:25:37 +0300

    add page-header for `contacts` page (#22)

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,5 +1,12 @@
+import { PageHeader } from '@/widgets'
+import styles from './page.module.scss'
+
 function ContactsPage() {
-  return <div>Контакты</div>
+  return (
+    <main>
+      <PageHeader title="Контакты" className={styles.pageHeader} shadowClassName={styles.pageHeader__shadow} />
+    </main>
+  )
 }
 
 export default ContactsPage
