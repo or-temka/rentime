8afb5e494a1c6f52e2008658e8be2f89979383da
artem schegortzov <tema.chegortzov@mail.ru>
2025-04-28 23:22:28 +0300

    Add MainContentContainer component

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,10 +1,19 @@
 import { PageHeader } from '@/widgets'
 import styles from './page.module.scss'
+import { MainContentContainer } from '@/shared/components'
 
 function ContactsPage() {
   return (
     <main>
-      <PageHeader title="Контакты" className={styles.pageHeader} shadowClassName={styles.pageHeader__shadow} />
+      <PageHeader
+        title="Контакты"
+        className={styles.pageHeader}
+        shadowClassName={styles.pageHeader__shadow}
+      />
+
+      <MainContentContainer>
+        
+      </MainContentContainer>
     </main>
   )
 }
