76e1de141a43098d9a495b236a1d536c01a83800
artem schegortzov <tema.chegortzov@mail.ru>
2025-04-30 00:19:28 +0300

    Add classNames

--- src/app/lease-terms/_components/RentRules/Rule.tsx
+++ src/app/lease-terms/_components/RentRules/Rule.tsx
@@ -1,10 +1,11 @@
 import { RuleProps } from './types'
+import styles from './rule.module.scss'
 
 export const Rule = ({ title, children }: RuleProps) => {
   return (
-    <article>
-      <h2>{title}</h2>
-      <div>{children}</div>
+    <article className={styles.rule}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.content}>{children}</div>
     </article>
   )
 }
--- src/app/lease-terms/_components/RentRules/types.ts
+++ src/app/lease-terms/_components/RentRules/types.ts
@@ -1,6 +1,11 @@
-import { ReactNode } from 'react'
+import { DetailedHTMLProps, LiHTMLAttributes, ReactNode } from 'react'
 
 export type RuleProps = {
   title: string
   children: ReactNode
 }
+
+export type UlLiProps = {} & DetailedHTMLProps<
+  LiHTMLAttributes<HTMLLIElement>,
+  HTMLLIElement
+>
