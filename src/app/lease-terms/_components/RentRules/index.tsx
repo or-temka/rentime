72e9ffa367b83b226c0a27ccccd3c49ad2b284df
artem schegortzov <tema.chegortzov@mail.ru>
2025-04-29 23:59:31 +0300

    Add rent rules skeleton

---
+++ src/app/lease-terms/_components/RentRules/index.tsx
@@ -0,0 +1,3 @@
+export const RentRules = () => {
+  return <div>index</div>
+}
--- src/app/lease-terms/page.tsx
+++ src/app/lease-terms/page.tsx
@@ -1,13 +1,16 @@
 import { MainContentContainer } from '@/shared/components'
 import styles from './page.module.scss'
 import { ThinPageHeader } from '@/widgets'
+import { RentRules } from './_components'
 
 function LeaseTermsPage() {
   return (
     <main>
       <ThinPageHeader title="Условия аренды" className={styles.pageHeader} />
 
-      <MainContentContainer></MainContentContainer>
+      <MainContentContainer>
+        <RentRules />
+      </MainContentContainer>
     </main>
   )
 }
