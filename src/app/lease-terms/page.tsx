1f499695fc868eb2c8d4fe31ddaea6da82f0e238
artem schegortzov <tema.chegortzov@mail.ru>
2025-04-29 23:57:40 +0300

    Add ThinPageHeader component & add for pages

--- src/app/lease-terms/page.tsx
+++ src/app/lease-terms/page.tsx
@@ -1,5 +1,15 @@
+import { MainContentContainer } from '@/shared/components'
+import styles from './page.module.scss'
+import { ThinPageHeader } from '@/widgets'
+
 function LeaseTermsPage() {
-  return <div>Условия аренды</div>
+  return (
+    <main>
+      <ThinPageHeader title="Условия аренды" className={styles.pageHeader} />
+
+      <MainContentContainer></MainContentContainer>
+    </main>
+  )
 }
 
 export default LeaseTermsPage
