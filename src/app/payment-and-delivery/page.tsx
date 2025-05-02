88de1df151ec0ea70429f33f9562dcecf8b8df85
artem schegortzov <tema.chegortzov@mail.ru>
2025-05-02 17:44:13 +0300

    Add header

--- src/app/payment-and-delivery/page.tsx
+++ src/app/payment-and-delivery/page.tsx
@@ -1,5 +1,15 @@
+import { ThinPageHeader } from '@/widgets'
+import styles from './page.module.scss'
+import { MainContentContainer } from '@/shared/components'
+
 function PaymentAndDeliveryPage() {
-  return <div>Оплата и доставка</div>
+  return (
+    <div>
+      <ThinPageHeader title="Оплата и доставка" className={styles.pageHeader} />
+
+      <MainContentContainer>Контент</MainContentContainer>
+    </div>
+  )
 }
 
 export default PaymentAndDeliveryPage
