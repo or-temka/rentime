5d5b6622004f27ca0b0fbaaa2538b2a573e19da0
artem schegortzov <tema.chegortzov@mail.ru>
2025-05-02 16:43:02 +0300

    Done burger menu for mobile

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,11 +1,35 @@
+'use client'
+
 import styles from './index.module.scss'
 import { navigationElements } from './constants'
 import Link from 'next/link'
 import { ROUTES } from '@/config/routes'
 import { Button } from '../Button'
 import { Logo } from '../svgs'
+import { useEffect, useState } from 'react'
 
 export const Header = () => {
+  const [showMenuModal, setShowMenuModal] = useState(false)
+
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    setVh()
+    window.addEventListener('resize', setVh)
+    return () => window.removeEventListener('resize', setVh)
+  }, [])
+
+  useEffect(() => {
+    if (showMenuModal) {
+      document.body.classList.add('no-scroll')
+    } else {
+      document.body.classList.remove('no-scroll')
+    }
+  }, [showMenuModal])
+
   return (
     <header className={styles.header}>
       <Link href={ROUTES.HOME} className={styles.logoContainer}>
@@ -29,12 +53,50 @@ export const Header = () => {
         >
           Написать
         </Button>
-        <div className={styles.mobileMenuBtn}>
+        <div
+          className={styles.mobileMenuBtn}
+          onClick={() => setShowMenuModal(true)}
+        >
           <div className={styles.mobileMenuBtn__item} />
           <div className={styles.mobileMenuBtn__item} />
           <div className={styles.mobileMenuBtn__item} />
         </div>
       </nav>
+
+      {showMenuModal && (
+        <div className={styles.mobileMenuModal}>
+          <div
+            className={styles.mobileMenuModal__fill}
+            onClick={() => setShowMenuModal(false)}
+          />
+          <aside className={styles.aside}>
+            <nav className={styles.mobileNavigation}>
+              <div className={styles.aside__labelContainer}>
+                <span className={styles.aside__label}>Меню</span>
+              </div>
+              <ul className={styles.mobileNavigation__ul}>
+                {navigationElements.map((navElem, index) => (
+                  <Link
+                    onClick={() => setShowMenuModal(false)}
+                    href={navElem.link}
+                    key={index}
+                  >
+                    <li className={styles.mobileNavigation__li}>
+                      {navElem.label}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </nav>
+            <div
+              className={styles.aside__closeBtnContainer}
+              onClick={() => setShowMenuModal(false)}
+            >
+              <span className={styles.aside__closeBtn}>Закрыть</span>
+            </div>
+          </aside>
+        </div>
+      )}
     </header>
   )
 }
