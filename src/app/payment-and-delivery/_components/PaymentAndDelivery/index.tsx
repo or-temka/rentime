80c059f3c80d69680650ce0ba68b372696ee06de
artem schegortzov <tema.chegortzov@mail.ru>
2025-05-02 18:32:08 +0300

    Add segmented select for payment and delivery

--- src/app/payment-and-delivery/_components/PaymentAndDelivery/index.tsx
+++ src/app/payment-and-delivery/_components/PaymentAndDelivery/index.tsx
@@ -1,5 +1,25 @@
+'use client'
+
+import { SegmentedSelect, SegmentedSelectOption } from '@/shared/components'
+import { useState } from 'react'
+import { PAYMENT_AND_DELIVERY_TABS } from './constants'
+
 export const PaymentAndDelivery = () => {
-  return <section>
-    
-  </section>
+  const [activeTab, setActiveTab] = useState<SegmentedSelectOption>(
+    PAYMENT_AND_DELIVERY_TABS[0]
+  )
+
+  return (
+    <section>
+      <SegmentedSelect
+        options={PAYMENT_AND_DELIVERY_TABS}
+        activeOption={activeTab}
+        setActiveOption={setActiveTab}
+      />
+
+      {activeTab.value === 'delivery' && <div>Доставка</div>}
+
+      {activeTab.value === 'payment' && <div>Оплата</div>}
+    </section>
+  )
 }
