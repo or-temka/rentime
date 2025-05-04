39b57aaed5ee9e655830377f789d11b82dcc7ec6
Artem Shchegortzov <69795850+or-temka@users.noreply.github.com>
2025-05-04 13:12:47 +0300

    Update `collapse` component styles (#35)
    * Updated
    
    * Otimized for mobile

--- src/shared/components/Collapse/index.tsx
+++ src/shared/components/Collapse/index.tsx
@@ -9,16 +9,21 @@ export const Collapse = ({ title, children }: CollapseProps) => {
   const [isOpen, setIsOpen] = useState(false)
   const contentRef = useRef<HTMLDivElement>(null)
   const [height, setHeight] = useState('0px')
+  const [svgRotate, setSvgRotate] = useState(0)
 
   useEffect(() => {
     setHeight(isOpen ? `${contentRef.current?.scrollHeight}px` : '0px')
+    setSvgRotate(isOpen ? 45 : 0)
   }, [isOpen])
 
   return (
     <div className={styles.container}>
       <button className={styles.header} onClick={() => setIsOpen(!isOpen)}>
         {title}
-        <CrossSVG className={styles.cross} />
+        <CrossSVG
+          className={styles.cross}
+          style={{ transform: `rotate(${svgRotate}deg)` }}
+        />
       </button>
       <div ref={contentRef} className={styles.content} style={{ height }}>
         <div className={styles.body}>{children}</div>
