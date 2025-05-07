a15a8ccadcdceb2eaff186ed4d57773df076405d
Artem Shchegortzov <69795850+or-temka@users.noreply.github.com>
2025-05-07 23:54:54 +0300

    Add `page header` for product page (#41)

--- src/app/products/[product-slug]/page.tsx
+++ src/app/products/[product-slug]/page.tsx
@@ -1,5 +1,22 @@
+import { MainContentContainer, RentButton } from '@/shared/components'
+import { PageHeader } from '@/widgets'
+import styles from './page.module.scss'
+
 function ProductPage() {
-  return <div>Товар</div>
+  return (
+    <main>
+      <PageHeader
+        title="PlayStation 5"
+        info={{
+          text: 'Мощная игровая консоль нового поколения с невероятной графикой, быстрой загрузкой и полным погружением в игровой процесс',
+          button: <RentButton />,
+        }}
+        className={styles.pageHeader}
+      />
+
+      <MainContentContainer></MainContentContainer>
+    </main>
+  )
 }
 
 export default ProductPage
