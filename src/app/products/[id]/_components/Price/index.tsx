dfc45072379a1b883ef95c3965837061595d6efe
artem schegortzov <tema.chegortzov@mail.ru>
2025-05-13 23:37:31 +0300

    Add styles structure

--- src/app/products/[id]/_components/Price/index.tsx
+++ src/app/products/[id]/_components/Price/index.tsx
@@ -1,5 +1,6 @@
 import { H2 } from '@/shared/components'
 import styles from './index.module.scss'
+import pricePerDayStyles from './pricePerDay.module.scss'
 import { ROUTES } from '@/config/routes'
 import { PriceProps } from './types'
 
@@ -15,16 +16,20 @@ const PricePerDay = ({
   if (day > 5) return <></>
 
   return (
-    <div>
-      <div>
-        <span>{dayName}</span>
+    <div className={pricePerDayStyles.container}>
+      <div className={pricePerDayStyles.day}>
+        <span className={pricePerDayStyles.day__text}>{dayName}</span>
       </div>
-      <div>
-        <div>
-          <span>{pricePerDay} руб./сутки</span>
+      <div className={pricePerDayStyles.content}>
+        <div className={pricePerDayStyles.perDay}>
+          <span className={pricePerDayStyles.perDay__text}>
+            {pricePerDay} руб./сутки
+          </span>
         </div>
-        <div>
-          <span>{pricePerDay * day} руб</span>
+        <div className={pricePerDayStyles.total}>
+          <span className={pricePerDayStyles.total__text}>
+            {pricePerDay * day} руб
+          </span>
         </div>
       </div>
     </div>
@@ -33,29 +38,35 @@ const PricePerDay = ({
 
 export const Price = ({ product }: PriceProps) => {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.information}>
         <H2>Стоимость</H2>
-        <div>
-          <p>
+        <div className={styles.infoContent}>
+          <p className={styles.paragraph}>
             Стоимость аренды рассчитывается в зависимости от количества дней,
             которые вы планируете использовать товар. Чем дольше аренда, тем
             выгоднее цена за день.
           </p>
-          <div>
-            <span>
+          <div className={styles.additional}>
+            <span className={styles.additional__text}>
               * Арендовать можно на любой срок (+1 день свыше недели /800 руб.)
             </span>
-            <span>
+            <span className={styles.additional__text}>
               ** Доставка по всему городу: 100-600 рублей в зависимости от
               района (
-              <a href={ROUTES.PAYMENT_AND_DELIVERY}>узнать цену доставки</a>)
+              <a
+                href={ROUTES.PAYMENT_AND_DELIVERY}
+                className={styles.deliveryLink}
+              >
+                узнать цену доставки
+              </a>
+              )
             </span>
           </div>
         </div>
       </div>
 
-      <div>
+      <div className={styles.prices}>
         {Object.entries(product.price.perDay).map(([key, value], index) => (
           <PricePerDay day={index + 1} pricePerDay={value} key={key} />
         ))}
