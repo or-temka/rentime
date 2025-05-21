6ce8bad0803e2686569d06a90212e5094a765f45
artem schegortzov <tema.chegortzov@mail.ru>
2025-05-21 22:22:07 +0300

    Done class names structure

--- src/app/products/[id]/_components/Advertisement/index.tsx
+++ src/app/products/[id]/_components/Advertisement/index.tsx
@@ -2,17 +2,23 @@ import { ROUTES } from '@/config/routes'
 import { RentButton, UnderlineSpan } from '@/shared/components'
 import { TextsBackgound } from '@/widgets'
 import Link from 'next/link'
+import styles from './index.module.scss'
 
 export const Advertisement = () => {
   return (
-    <section>
+    <section className={styles.section}>
       <TextsBackgound />
 
-      <div>
-        <span>Арендуй. Пользуйся. Возвращай. Легко!</span>
-        <div>
+      <div className={styles.content}>
+        <span className={styles.slogan}>
+          Арендуй. Пользуйся. Возвращай. Легко!
+        </span>
+        <div className={styles.rent}>
           <RentButton theme="dark" />
-          <Link href={ROUTES.PAYMENT_AND_DELIVERY}>
+          <Link
+            href={ROUTES.PAYMENT_AND_DELIVERY}
+            className={styles.paymentAndDeliveryLink}
+          >
             <UnderlineSpan>Оплата и доставка</UnderlineSpan>
           </Link>
         </div>
