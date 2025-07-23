78fb1ed3b5f2fcf116800af8d2eb693f9e7b9f54
artem schegortzov <tema.chegortzov@mail.ru>
2025-07-23 23:28:32 +0300

    Update PS5 games

--- src/entities/product/constants/products/playstation5/availableGames.ts
+++ src/entities/product/constants/products/playstation5/availableGames.ts
@@ -1,7 +1,6 @@
 import { Games, PLAYSTATION_5_GAMES } from '@/entities/game'
 
 export const AVAILABLE_PLAYSTATION_5_GAMES: Games = [
-  PLAYSTATION_5_GAMES['split-fiction'],
   PLAYSTATION_5_GAMES['ufc-5'],
   PLAYSTATION_5_GAMES['it-takes-two'],
   PLAYSTATION_5_GAMES['nhl-25'],
@@ -9,9 +8,7 @@ export const AVAILABLE_PLAYSTATION_5_GAMES: Games = [
   PLAYSTATION_5_GAMES['a-way-out'],
   PLAYSTATION_5_GAMES['mortal-kombat-1'],
   PLAYSTATION_5_GAMES['god-of-war-ragnarok'],
-  PLAYSTATION_5_GAMES['resident-evil-village'],
   PLAYSTATION_5_GAMES['spider-man-miles-morales'],
   PLAYSTATION_5_GAMES['uncharted-4'],
   PLAYSTATION_5_GAMES['god-of-war-2018'],
-  PLAYSTATION_5_GAMES['brothers-a-tale-of-two-sons'],
 ]
