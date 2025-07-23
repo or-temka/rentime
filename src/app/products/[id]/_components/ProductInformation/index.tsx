dd3502a057e56476d19a6a1d5fda72dc076075b9
artem schegortzov <tema.chegortzov@mail.ru>
2025-07-24 00:39:18 +0300

    Add redirect for lease and terms in product page

--- src/app/products/[id]/_components/ProductInformation/index.tsx
+++ src/app/products/[id]/_components/ProductInformation/index.tsx
@@ -3,15 +3,23 @@
 import { informationSelectOptions } from './constants'
 import { SegmentedSelect, SegmentedSelectOption } from '@/shared/components'
 import styles from './index.module.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Price } from '../Price'
 import { ProductInformationProps } from './types'
 import { Equipment } from '../Equipment'
+import { ROUTES } from '@/config/routes'
 
 export const ProductInformation = ({ product }: ProductInformationProps) => {
   const [activeSection, setActiveSection] = useState<SegmentedSelectOption>(
     informationSelectOptions[0]
   )
+
+  useEffect(() => {
+    if (activeSection.value === 'lease terms') {
+      window.open(ROUTES.LEASE_TERMS.BASE, '_blank', 'noopener,noreferrer')
+      setActiveSection(informationSelectOptions[0])
+    }
+  }, [activeSection])
 
   return (
     <section className={styles.container}>
