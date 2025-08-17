0fc7219ab9674d8f4cff0c117c54fa9ca831c32c
Artem Shchegortzov <69795850+or-temka@users.noreply.github.com>
2025-08-17 20:21:21 +0300

    Add `smart hover` effect (#91)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import path from 'path'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), 'src/app')],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
