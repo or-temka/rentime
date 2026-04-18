7156ddc18dd13dc383664441b543afa7dcb41497
artem schegortzov <tema.chegortzov@mail.ru>
2026-04-18 20:56:18 +0300

    Обновлена кнопка позвонить

--- src/shared/components/PhoneButton/index.tsx
+++ src/shared/components/PhoneButton/index.tsx
@@ -7,6 +7,9 @@ export const PhoneButton = () => {
     <a target="_blank" href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUMBER}`}>
       <Button className={styles.button}>
         <PhoneSVG height={18} />
+        <span className={styles.button__numberText}>
+          {process.env.NEXT_PUBLIC_PHONE_NUMBER_STRING}
+        </span>
       </Button>
     </a>
   )
